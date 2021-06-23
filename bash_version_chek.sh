97581e8957606ae4c0b32984a3008be09a5df862
shashikanth <shashikanthvarma98@gmail.com>
2021-06-23 04:22:34 +0000

    Update bash_version_chek.sh

--- bash_version_chek.sh
+++ bash_version_chek.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 #this programme displays the version of bash currnently run
 
+noclr='\033[0m'
+grn='033[0;32m'  #hex codes for different colours
+blu='033[0;34m'
 ani(){
-   echo -ne "cheking version" 
+   echo -ne "${grn}cheking version" 
    i=0
    while ((i<=10))
    do
 	  echo -ne "."
 
 	  i=$i+1
-	  sleep 0.2
+	  sleep 0.2  # adding sllep
    done 
 
 }
 getversion(){
 	v=$(bash --version | grep 'GNU bash' | awk '{print substr($4,1,1)}')
 	echo ""
-	echo  $v
+	echo -e "${blu}$v${noclr}"
 
+   #-e is for changing color
+   
 }
 
 ani
